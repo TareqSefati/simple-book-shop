ff08e74280a884946bc54bc091ad95899153302b
Tareq Sefati <tareq.sefati@gmail.com>
2024-09-25 02:11:04 +0600

    Working on react router.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 export const ROUTES = {
     HOME: "/",
-    PRODUCTS: "/products",
-    SINGLE_PRODUCTS: {
-      STATIC: "/product/:productId",
-      DYNAMIC: (productId) => `/product/${productId}`,
-    },
+    ABOUT: "/about",
+    BLOG: "/blog",
+    FAQ: "/faq",
   };
   
