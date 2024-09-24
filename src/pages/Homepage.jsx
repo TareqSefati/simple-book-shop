b13f9c10b2f9d1024e8311a90681ed2efd41bd16
Tareq Sefati <tareq.sefati@gmail.com>
2024-09-25 01:25:51 +0600

    Implement navbar secction.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,13 @@
+import Banner from "../components/Banner";
+import BookList from "../components/book/BookList";
+import Navbar from "../components/shared/Navbar";
+
 export default function Homepage() {
-	return <div>Homepage</div>;
+	return (
+		<div className="container mx-auto">
+			<Navbar />
+            <Banner />
+            <BookList />
+		</div>
+	);
 }
