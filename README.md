fb9ba41fd156a397224607229526c2041350aebe
Tareq Sefati <tareq.sefati@gmail.com>
2024-09-25 07:16:31 +0600

    Update readme file and make functional book details buttons.

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Simple Book Shop Website
 
 This is a simple responsive, multi page based website using React and React Router.
+
+## Technology used
+
+-   React, React Router
+-   Tailwind
+-   DaisyUI
+
+## Webpage Sections:
+
+1. Navbar section
+2. Banner section
+3. Fetch Data from local file
+4. Show all data in grid view
+5. Show single data using react router
+6. FAQ section
+7. Footer section
+
+-   Live URL:
--- src/components/book/BookDetails.jsx
+++ src/components/book/BookDetails.jsx
@@ -1,58 +1,47 @@
 export default function BookDetails({ book }) {
 	return (
-        <div className="card card-side bg-base-100 shadow-xl mt-10 p-4">
-            <figure>
-                <img src={book.image} alt="book-image" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    Book Name: {book.bookName}
-                </h2>
-                <p>Author: {book.author} </p>
-                <p>Category: {book.category} </p>
-                <p>Review: {book.review} </p>
-                <p>Tags: {book.tags} </p>
-                <p>Total Pages: {book.totalPages} </p>
-                <p>Publisher: {book.publisher} </p>
-                <p>Year Of Publishing: {book.yearOfPublishing} </p>
-                <p>Rating: {book.rating} </p>
-                <p>Details</p>
-                <div className="card-actions justify-end">
-                    <button
-                        onClick={showToastWish}
-                        className="btn btn-neutral"
-                    >
-                        Wish to Read
-                    </button>
-                    <button
-                        onClick={()=>showToastCart(book.bookId, book.bookName)}
-                        className="btn btn-primary"
-                    >
-                        Add to Cart
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-function showToastCart(bookId, bookName) {
-    alert(bookId + " " +bookName);
-	
-		<div className="toast toast-end">
-			<div className="alert alert-success">
-				<span>Book is added to wish list. </span>
-			</div>
-		</div>
-	
-}
-
-function showToastWish() {
-	return (
-		<div className="toast toast-end">
-			<div className="alert alert-success">
-				<span>Book is added to cart.</span>
+		<div className="card card-side bg-base-100 shadow-xl mt-10 p-4">
+			<figure>
+				<img src={book.image} alt="book-image" />
+			</figure>
+			<div className="card-body">
+				<h2 className="card-title">Book Name: {book.bookName}</h2>
+				<p>Author: {book.author} </p>
+				<p>Category: {book.category} </p>
+				<p>Review: {book.review} </p>
+				<p>Tags: {book.tags} </p>
+				<p>Total Pages: {book.totalPages} </p>
+				<p>Publisher: {book.publisher} </p>
+				<p>Year Of Publishing: {book.yearOfPublishing} </p>
+				<p>Rating: {book.rating} </p>
+				<p>Details</p>
+				<div className="card-actions justify-end">
+					<button
+						onClick={() =>
+							showToastWish(book.bookId, book.bookName)
+						}
+						className="btn btn-neutral"
+					>
+						Wish to Read
+					</button>
+					<button
+						onClick={() =>
+							showToastCart(book.bookId, book.bookName)
+						}
+						className="btn btn-primary"
+					>
+						Add to Cart
+					</button>
+				</div>
 			</div>
 		</div>
 	);
+}
+
+function showToastCart(bookId, bookName) {
+	alert("BookId: " + bookId + " - " + bookName + " added to your cart.");
+}
+
+function showToastWish(bookId, bookName) {
+	alert("BookId: " + bookId + " - " + bookName + " added to your wish list");
 }
